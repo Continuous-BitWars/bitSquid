d1d24b79b69ea3b7bd3277767de5ae9a05da4298
Marcel Hartmann <mahait17@hs-esslingen.de>
2024-11-16 05:28:35 +0100

    changed routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,53 +1,57 @@
-import { Routes } from '@angular/router';
-import { MasterComponent } from './components/master/master.component';
-import { Game2Component } from './components/game/game.component';
-import { GamesPageComponent } from './pages/games-page/games-page.component';
-import { LeaguesPageComponent } from './pages/leagues-page/leagues-page.component';
-import { PlayersPageComponent } from './pages/players-page/players-page.component';
-import { MapsPageComponent } from './pages/maps-page/maps-page.component';
-import { PlayerDetailComponent } from './components/player-detail/player-detail.component';
-import { LeagueDetailComponent } from './components/league-detail/league-detail.component';
+import {Routes} from '@angular/router';
+import {MasterComponent} from './components/master/master.component';
+import {Game2Component} from './components/game/game.component';
+import {GamesPageComponent} from './pages/games-page/games-page.component';
+import {LeaguesPageComponent} from './pages/leagues-page/leagues-page.component';
+import {PlayersPageComponent} from './pages/players-page/players-page.component';
+import {MapsPageComponent} from './pages/maps-page/maps-page.component';
+import {PlayerDetailComponent} from './components/player-detail/player-detail.component';
+import {LeagueDetailComponent} from './components/league-detail/league-detail.component';
+import {GameDetailsComponent} from './game-details/game-details.component';
 //import { EmployeeComponent } from './components/employee/employee.component';
 //import { ClientComponent } from './components/client/client.component';
-import {GameDetailsComponent} from './game-details/game-details.component';
 
 
 export const routes: Routes = [
-    {
-        path:'',
-        redirectTo:'games',
-        pathMatch:'full'
-    },
-    {
-        path:'master',
-        component:MasterComponent
-    },
-    {
-        path:'games',
-        component:GamesPageComponent
-    },
-    {
-        path:'game',
-        component:Game2Component
-    },
-    {
-        path:'leagues',
-        component:LeaguesPageComponent
-    },
-    {
-        path:'leagues/:leagueId/scoreboard',
-        component:LeagueDetailComponent
-    },
-    {
-        path:'players',
-        component:PlayersPageComponent
-    },
-    {
-        path:'players/:id/player',
-        component:PlayerDetailComponent
-    },
-    {
-        path:'maps',
-        component:MapsPageComponent
-    }
+  {
+    path: 'games',
+    redirectTo: 'games',
+    pathMatch: 'full'
+  },
+  {
+    path: 'master',
+    component: MasterComponent
+  },
+  {
+    path: 'games',
+    component: GamesPageComponent
+  },
+  {
+    path: 'game',
+    component: Game2Component
+  },
+  {
+    path: 'games/:gameId',
+    component: GameDetailsComponent
+  },
+  {
+    path: 'leagues',
+    component: LeaguesPageComponent
+  },
+  {
+    path: 'leagues/:leagueId/scoreboard',
+    component: LeagueDetailComponent
+  },
+  {
+    path: 'players',
+    component: PlayersPageComponent
+  },
+  {
+    path: 'players/:id/player',
+    component: PlayerDetailComponent
+  },
+  {
+    path: 'maps',
+    component: MapsPageComponent
+  }
 ];
